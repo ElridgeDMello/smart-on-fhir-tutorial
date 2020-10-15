3f10befc8ad5b5026e05d0e4832b3db00ef65116
Elridge D'Mello <elridge.dmello@gmail.com>
2020-10-15 16:25:47 -0500

    log patient object

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -11,6 +11,7 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
+        console.log(`patient data :${JSON.stringify(pt)}`)
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
