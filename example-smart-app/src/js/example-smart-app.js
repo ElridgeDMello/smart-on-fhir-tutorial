5011ab4147e625276c4bf8c576bb6282ebacf23d
Elridge D'Mello <elridge.dmello@gmail.com>
2020-10-22 13:34:10 -0500

    eslint

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,66 +1,76 @@
-(function(window){
+(function(window) {
   var smartApiFunctions = null;
   window.extractData = function() {
     var ret = $.Deferred();
 
     function onError() {
-      console.log('Loading error', arguments);
+      console.log("Loading error", arguments);
       ret.reject();
     }
 
     function getSmartApiFunctions({ smart }) {
-        var functions = {
-            writeMName: function({ patient }) {
-                var ptId = patient.id;
-                console.log(`TODO: write middle name using smart api to patient id: `, ptId)
-                patient.active = !patient.active
-                var updatePromise = smart.api.update({ resource: patient })
-                console.log(`update called`)
-                $.when(updatePromise).done(function(r) {
-                  console.log(`updated: `, r)
-                })
-                $.when(updatePromise).fail(function(err) { console.log(`ERROR: `, err); })
-            }
+      var functions = {
+        writeMName: function({ patient }) {
+          var ptId = patient.id;
+          console.log(
+            `TODO: write middle name using smart api to patient id: `,
+            ptId
+          );
+          patient.active = !patient.active;
+          var updatePromise = smart.api.update({ resource: patient });
+          console.log(`update called`);
+          $.when(updatePromise).done(function(r) {
+            console.log(`updated: `, r);
+          });
+          $.when(updatePromise).fail(function(err) {
+            console.log(`ERROR: `, err);
+          });
         }
-        return functions
+      };
+      return functions;
     }
 
-    function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {
+    function onReady(smart) {
+      if (smart.hasOwnProperty("patient")) {
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+          type: "Observation",
+          query: {
+            code: {
+              $or: [
+                "http://loinc.org|8302-2",
+                "http://loinc.org|8462-4",
+                "http://loinc.org|8480-6",
+                "http://loinc.org|2085-9",
+                "http://loinc.org|2089-1",
+                "http://loinc.org|55284-4"
+              ]
+            }
+          }
+        });
 
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
-          console.log(`patient data :${JSON.stringify(patient)}`)
-          var byCodes = smart.byCodes(obv, 'code');
-          console.log(`byCodes: ${JSON.stringify(byCodes)}`)
+          console.log(`patient data :${JSON.stringify(patient)}`);
+          var byCodes = smart.byCodes(obv, "code");
+          console.log(`byCodes: ${JSON.stringify(byCodes)}`);
           var gender = patient.gender;
 
-          var fname = '';
-          var lname = '';
+          var fname = "";
+          var lname = "";
 
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+          if (typeof patient.name[0] !== "undefined") {
+            fname = patient.name[0].given.join(" ");
+            lname = patient.name[0].family.join(" ");
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          var height = byCodes("8302-2");
+          var systolicbp = getBloodPressureValue(byCodes("55284-4"), "8480-6");
+          var diastolicbp = getBloodPressureValue(byCodes("55284-4"), "8462-4");
+          var hdl = byCodes("2085-9");
+          var ldl = byCodes("2089-1");
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -69,18 +79,18 @@
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
 
-          if (typeof systolicbp != 'undefined')  {
+          if (typeof systolicbp !== "undefined") {
             p.systolicbp = systolicbp;
           }
 
-          if (typeof diastolicbp != 'undefined') {
+          if (typeof diastolicbp !== "undefined") {
             p.diastolicbp = diastolicbp;
           }
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
 
-          smartApiFunctions = getSmartApiFunctions({ smart, patient: p })
+          smartApiFunctions = getSmartApiFunctions({ smart, patient: p });
           ret.resolve(p);
         });
       } else {
@@ -90,27 +100,26 @@
 
     FHIR.oauth2.ready(onReady, onError);
     return ret.promise();
-
   };
 
-  function defaultPatient(){
+  function defaultPatient() {
     return {
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
+      fname: { value: "" },
+      lname: { value: "" },
+      gender: { value: "" },
+      birthdate: { value: "" },
+      height: { value: "" },
+      systolicbp: { value: "" },
+      diastolicbp: { value: "" },
+      ldl: { value: "" },
+      hdl: { value: "" }
     };
   }
 
   function getBloodPressureValue(BPObservations, typeOfPressure) {
     var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
+    BPObservations.forEach(function(observation) {
+      var BP = observation.component.find(function(component) {
         return component.code.coding.find(function(coding) {
           return coding.code == typeOfPressure;
         });
@@ -125,11 +134,13 @@
   }
 
   function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+    if (
+      typeof ob !== "undefined" &&
+      typeof ob.valueQuantity !== "undefined" &&
+      typeof ob.valueQuantity.value !== "undefined" &&
+      typeof ob.valueQuantity.unit !== "undefined"
+    ) {
+      return ob.valueQuantity.value + " " + ob.valueQuantity.unit;
     } else {
       return undefined;
     }
@@ -144,18 +155,19 @@
       }
     }
 
-    $('#holder').show();
-    $('#loading').hide();
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
-    document.getElementById("mnamebutton").addEventListener("click", callSmartApiWriteMName);
+    $("#holder").show();
+    $("#loading").hide();
+    $("#fname").html(p.fname);
+    $("#lname").html(p.lname);
+    $("#gender").html(p.gender);
+    $("#birthdate").html(p.birthdate);
+    $("#height").html(p.height);
+    $("#systolicbp").html(p.systolicbp);
+    $("#diastolicbp").html(p.diastolicbp);
+    $("#ldl").html(p.ldl);
+    $("#hdl").html(p.hdl);
+    document
+      .getElementById("mnamebutton")
+      .addEventListener("click", callSmartApiWriteMName);
   };
-
 })(window);
